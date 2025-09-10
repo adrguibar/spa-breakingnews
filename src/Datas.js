bb60cdd729db024138675ea43bf3bba5238a380e
adriano <adriano.a.barbosa@accenture.com>
2025-09-10 20:50:18 -0300

    feat: create mock data

---
+++ src/Datas.js
@@ -0,0 +1,23 @@
+export const news = [
+    {
+        title: "Noticia 1",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyfSlig5yj-0jcQr1ADwB54Vf3nLw-cc83Pg&s",
+        likes: 2,
+        comments: 4,
+    },
+    {
+        title: "Noticia 2",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyfSlig5yj-0jcQr1ADwB54Vf3nLw-cc83Pg&s",
+        likes: 2,
+        comments: 4,
+    },
+    {
+        title: "Noticia 3",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyfSlig5yj-0jcQr1ADwB54Vf3nLw-cc83Pg&s",
+        likes: 2,
+        comments: 4,
+    }
+]
