1257a5f1d056246163ec905a644131981670739e
adriano <adriano.a.barbosa@accenture.com>
2025-09-23 21:34:32 -0300

    refact: alterando estilo do top card

--- src/components/Cards/CardStyled.jsx
+++ src/components/Cards/CardStyled.jsx
@@ -2,31 +2,52 @@ import styled from "styled-components";
 
 export const CardContainer = styled.section`
   display: flex;
+  flex: 1 1 auto;
+  min-width: 0;
   flex-direction: column;
   gap: 1rem;
-  max-width: 100%;
+  /* max-width: 100%; */
 
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
     rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
   border-radius: 0.3rem;
   background-color: #fff;
-  padding: 2rem;
 `;
 
 export const CardBody = styled.article`
   display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
+  width: 100%;
+  height: 100%;
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 1rem;
+    width: 100%;  
+  }
 
   h2 {
     margin-bottom: 1rem;
+    margin-right: 1rem;
   }
 
   img {
-    width: 30%;
+    width: 40%;
     object-fit: cover;
     object-position: center;
+    border-radius: 0 .3rem .3rem 0;
+  }
+`;
+
+export const CardHeader = styled.article`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  font-size: ${(props) => (props.top ? "1.5rem" : ".9rem")};
+
+  h2 {
+    margin-bottom: 1rem;
+    font-size: ${(props) => (props.top ? "3rem" : "1rem")};
   }
 `;
 
@@ -35,7 +56,7 @@ export const CardFooter = styled.article`
   align-items: center;
   gap: 0.7rem;
 
-  div {
+  section {
     display: flex;
     align-items: center;
     gap: 0.2rem;
