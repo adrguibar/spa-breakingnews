430497ed235a379e231700ecef19a4e09621bce7
adriano <adriano.a.barbosa@accenture.com>
2025-09-23 21:35:13 -0300

    refact: alterando pagina da home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,49 @@
+import { useEffect, useState } from "react";
 import { Card } from "../../components/Cards/Card";
 import { Navbar } from "../../components/Navbar/Navbar";
-import { news } from "../../Datas";
-import { HomeBody } from "./HomeStyled";
+import { getAllPosts, getTopPosts } from "../../services/postsService";
+import { HomeBody, HomeHeader } from "./HomeStyled";
 
 export default function Home() {
+    const [posts, setPosts] = useState([]);
+    const [topPost, setTopPost] = useState({});
+
+    async function findPosts(){
+        // Chamar o serviço que busca as notícias
+        const postsResponse = await getAllPosts();
+        setPosts(postsResponse.data.results);
+
+        const topPostResponse = await getTopPosts()
+        setTopPost(topPostResponse.data.post)
+    }
+    
+    useEffect(() => {
+        findPosts();
+    }, []);
+    
   return (
     <>
       <Navbar />
+      <HomeHeader>
+        <Card
+            top={true}
+            title={topPost.title}
+            text={topPost.text}
+            banner={topPost.banner}
+            likes={topPost.likes}
+            comments={topPost.comments}
+        />
+      </HomeHeader>
       <HomeBody>
-        {news.map((item, index) => (
-          <Card key={index} news={item} />
+        {posts.map((item) => (
+          <Card 
+          key={item.id} 
+          title={item.title} 
+          text={item.text}
+          banner={item.banner}
+          likes={item.likes}
+          comments={item.comments}  
+          />
         ))}
       </HomeBody>
     </>
