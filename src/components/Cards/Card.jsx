ebf11c7f3ba91d3aa839b8a090ae02c1466f9ca5
adriano <adriano.a.barbosa@accenture.com>
2025-09-23 21:34:55 -0300

    refact: alterando component do card

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,28 +1,31 @@
-import { CardBody, CardContainer, CardFooter } from "./CardStyled";
+import { TextLimit } from "../TextLimit/TextLimit";
+import { CardBody, CardContainer, CardFooter, CardHeader } from "./CardStyled";
 
-export function Card({ news }) {
-  console.log(news);
+export function Card(props) {
   return (
     <CardContainer>
       <CardBody>
         <div>
-          <h2>{news.title}</h2>
-          <p>{news.text}</p>
+          <CardHeader top={props.top}>
+            <h2>{props.title}</h2>
+            <TextLimit text={props.text} limit={150}></TextLimit>
+          </CardHeader>
+
+          <CardFooter>
+            <section>
+              <span>{props.likes?.length}</span>
+              <i className="bi bi-hand-thumbs-up"></i>
+            </section>
+
+            <section>
+              <span>{props.comments?.length}</span>
+              <i className="bi bi-chat-square"></i>
+            </section>
+          </CardFooter>
         </div>
-        <img src={news.image} alt="Imagem" />
+
+        <img src={props.banner} alt="Imagem" />
       </CardBody>
-
-      <CardFooter>
-        <div>
-          <span>{news.likes}</span>
-          <i class="bi bi-hand-thumbs-up"></i>
-        </div>
-
-        <div>
-          <span>{news.comments}</span>
-          <i class="bi bi-chat-square"></i>
-        </div>
-      </CardFooter>
     </CardContainer>
   );
 }
